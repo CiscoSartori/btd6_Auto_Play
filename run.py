545fbf7cbbf1bdb0978cae0c51d226f2414ec0cd
CiscoSartori <lulucaantono@gmail.com>
2023-02-28 16:10:01 -0300

    select the bonus map

--- run.py
+++ run.py
@@ -19,7 +19,24 @@ def move_and_click_it(point):
 def locateCenterOnScreen(img_location):
 	location = pyautogui.locateCenterOnScreen(img_location)
 	return (location.x, location.y) if location else "pika"
-pyautogui.hotkey("alt","tab")
-sleep(1)
-print(pyautogui.locateCenterOnScreen('resources/event/play2.png', confidence=0.9))
-print()
+# pyautogui.hotkey("alt","tab")
+# sleep(1)
+# print(pyautogui.locateCenterOnScreen('resources/event/bonus.png', confidence=0.9))
+
+def start():
+	point = pyautogui.locateCenterOnScreen('resources/event/play.png')
+	pyautogui.click(point.x, point.y)
+	sleep(0.5)
+
+	bonus = pyautogui.locateCenterOnScreen('resources/event/bonus.png',confidence=0.9)
+	while bonus == None:
+		sleep(0.5)
+		right = pyautogui.locateCenterOnScreen('resources/map/right.png',confidence=0.9)
+		print(right.x, right.y)
+		pyautogui.click(right.x, right.y)
+		sleep(0.5)
+		bonus = pyautogui.locateCenterOnScreen('resources/event/bonus.png',confidence=0.9)
+		print("foi de americanas")
+	pyautogui.click(bonus.x, bonus.y)
+	
+start()
