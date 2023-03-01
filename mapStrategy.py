c89d5a832461b50f206e24d34fed4b0a94ac9f3e
CiscoSartori <lulucaantono@gmail.com>
2023-02-28 21:16:03 -0300

    no functional yet

---
+++ mapStrategy.py
@@ -0,0 +1,6 @@
+import argparse
+import json
+from time import sleep
+import datetime
+import pyautogui
+
--- run.py
+++ run.py
@@ -3,40 +3,31 @@ import json
 from time import sleep
 import datetime
 import pyautogui
-
-
-# class Point:
-# 	def __init__(self, x, y):
-# 		self.x = x
-# 		self.y = y
-		
-def move_mouse_to(point):
-	pyautogui.moveTo(point.x, point.y)
-	
-def move_and_click_it(point):
-	pyautogui.click(point.x, point.y)
-
-def locateCenterOnScreen(img_location):
-	location = pyautogui.locateCenterOnScreen(img_location)
-	return (location.x, location.y) if location else "pika"
-# pyautogui.hotkey("alt","tab")
-# sleep(1)
-# print(pyautogui.locateCenterOnScreen('resources/event/bonus.png', confidence=0.9))
+from function import Function
 
 def start():
-	point = pyautogui.locateCenterOnScreen('resources/event/play.png')
-	pyautogui.click(point.x, point.y)
-	sleep(0.5)
+
+	Function.locateClick('resources/event/play.png')
 
 	bonus = pyautogui.locateCenterOnScreen('resources/event/bonus.png',confidence=0.9)
 	while bonus == None:
 		sleep(0.5)
 		right = pyautogui.locateCenterOnScreen('resources/map/right.png',confidence=0.9)
-		print(right.x, right.y)
 		pyautogui.click(right.x, right.y)
 		sleep(0.5)
 		bonus = pyautogui.locateCenterOnScreen('resources/event/bonus.png',confidence=0.9)
-		print("foi de americanas")
 	pyautogui.click(bonus.x, bonus.y)
+	sleep(0.5)
+	
+	Function.locateClick('resources/difficulty/easy.png')
+
+	Function.locateClick('resources/difficulty/play.png')
 	
 start()
+
+
+
+
+
+
+# print(pyautogui.locateCenterOnScreen('resources/difficulty/easy.png', confidence=0.9))
